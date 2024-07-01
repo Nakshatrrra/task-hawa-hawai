5c3762b552c3cb36b965c1eba887e07830f253fc
Nakshatrrra <nakshatratrivedi44@gmail.com>
2024-07-01 12:41:47 +0530

    env

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
-// const dotenv =require('dotenv');
-// dotenv.config();
+const dotenv =require('dotenv');
+dotenv.config();
 const mysql = require('mysql2');
 
-// const {
-//     DB_HOST, DB_NAME, DB_USER, DB_PASSWORD,
-//   } = process.env;
+const {
+    DB_HOST, DB_NAME, DB_USER, DB_PASSWORD,
+  } = process.env;
 
 const pool = mysql.createPool({
-    host: "127.0.0.1",
-    user: "root",
-    password: "root",
-    database: "hava_havai"
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME
 });
 
 module.exports = pool.promise();
